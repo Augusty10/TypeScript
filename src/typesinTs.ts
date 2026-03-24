96e158d38eb8f77ca3284a3af14b09c31b5bcc3b
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-03-24 18:03:47 +0530

    add: annotation and inferencing
     today we learn about the  annotation and inferencing in TypeScript .

---
+++ src/typesinTs.ts
@@ -0,0 +1,13 @@
+let drink  = " chai "
+
+let  cups = Math.random() > 0.5 ?10:5
+
+// type infrenshing  
+
+let channleName =  " Chai Aur Code "
+
+// type anatotsion 
+
+let ChaiFlavour : string =" masalas chai "
+
+
