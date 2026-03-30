edaf1eb18ae4bd3aa32e7eb73cfbfc54b695f4c0
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-03-30 15:13:35 +0530

    update function .ts
    update function .ts

---
+++ src/function.ts
@@ -0,0 +1,31 @@
+function makeChai(type: string, cups: number){
+       console.log(`making ${cups} cups of ${type}`);
+       
+}
+
+makeChai(" masalsa Chai ", 2)
+
+
+function getChaiprice(type: string): number{
+       return 25;
+}
+
+function makeorder (order:string){
+     if(!order) return null
+     return order 
+}
+
+function logChai(): void{
+    console.log(" chai is ready ! ");
+}
+
+
+
+function Chai(order: { 
+         type: string;
+         sugar: number;
+         size: "small" | "medium " | "large"
+}): number {
+     return 4 
+
+}
